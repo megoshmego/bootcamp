b9542b854f2c116c0e6aeb9456258f5a11cf86a0
megoshmego <108840586+megoshmego@users.noreply.github.com>
2022-12-08 10:04:11 -0500

    Add files via upload

---
+++ springboard 18 python/18.2/number_compare.py
@@ -0,0 +1,13 @@
+def number_compare(a, b):
+        if a < b:
+            return "Second is greater"
+        elif a > b:
+            return "First is greater"
+        else: 
+            return "Numbers are equal"
+
+
+
+
+
+      

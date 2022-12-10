cf01a48f4757f83a0ed6dd96caa66ba0b8c752ea
megoshmego <108840586+megoshmego@users.noreply.github.com>
2022-12-10 07:21:07 -0500

    Add files via upload

---
+++ springboard 18 python/18.2 data structures/intersection.py
@@ -0,0 +1,34 @@
+def intersection(l1, l2):
+    """Return intersection of two lists as a new list::
+    
+        >>> intersection([1, 2, 3], [2, 3, 4])
+        [2, 3]
+        
+        >>> intersection([1, 2, 3], [1, 2, 3, 4])
+        [1, 2, 3]
+        
+        >>> intersection([1, 2, 3], [3, 4])
+        [3]
+        
+        >>> intersection([1, 2, 3], [4, 5, 6])
+        []
+    """
+    set2 = set(l2)
+
+    return [val for val in l1 if val in set2]
+
+"""THE ABOVE CONVERTS TO A SET... OR"""
+
+def merge(l1, l2):
+    return [val for val in l1 if val in l2]
+
+print(merge([1, 2, 6], [6, 4]))
+
+
+
+
+
+
+
+
+
